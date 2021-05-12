931f84ab9abe538e3c0c58eeadd7ed676dbc3ee1
THorvath1 <72626710+THorvath1@users.noreply.github.com>
2021-05-12 00:49:12 -0700

    Delete output.py

--- output.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def output_wrapper():
-    os.system('cls')
-    return 'this program will automatically accept your League of Legends queue, no more missing queues for you'
-
-def output_message(message):
-    print(output_wrapper())
-    print(message)
